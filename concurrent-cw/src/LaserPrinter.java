500c83478279fb22b7af6c26c85c639fba5008df
Thaveesha Dheerasekera <thaveesha.00@gmail.com>
2023-01-06 21:01:52 +0530

    Version 1.0 completed

--- concurrent-cw/src/LaserPrinter.java
+++ concurrent-cw/src/LaserPrinter.java
@@ -1,11 +1,11 @@
 /**
  * *************************************************************
- * File:	    LaserPrinter.java (Class)
- * Author:	    Thaveesha Dheerasekera; IIT ID: 2019483; UoW ID: w1761369
+ * File:        LaserPrinter.java (Class)
+ * Author:      Thaveesha Dheerasekera; IIT ID: 2019483; UoW ID: w1761369
  * Contents:    Concurrent Programming Coursework (2022/23)
  * Description: This provides the functions of printer. (Acts as the thread monitor)
- * Date:	    06-Jan-23
- * Version:	    1.0
+ * Date:        06-Jan-23
+ * Version:     1.0
  * *************************************************************
  */
 
